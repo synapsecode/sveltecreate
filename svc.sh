77683ba6943a37738c98d9a0713934feef4305a1
Manas Hejmadi | Developer <67999871+synapsecode@users.noreply.github.com>
2021-09-28 02:20:51 +0530

    Update svc.sh

--- svc.sh
+++ svc.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=$1
-MODE=$2
+FLAVOR=$2
 
 #Function to create svelte project and show output text
 svinstall () {
-	npx degit $1 $2
+	npx degit $1 $2 &> /dev/null #hides console output from command
 	echo "🟢 Created Svelte Template..."
 	cd $2
 	echo "🟢 Installing Dependencies..."
@@ -14,24 +14,26 @@ svinstall () {
 	echo "🎉 Svelte Project '$2' Created Successfully! 🎉"
 }
 
-
 # Check for Empty Project name
 if [[ $PROJECT_NAME == "" ]]; then
 	echo "❌ error: project name must not be empty!"
 	exit 0
 fi
 
-# Install Svelte based on whichever mode you want
-if [[ $MODE == "" ]]; then
-	# Regular Svelte App
+# Install Svelte based on whichever flavor you want
+if [[ $FLAVOR == "" ]]; then
+	# Vanilla Svelte App
 	svinstall 'sveltejs/template' $PROJECT_NAME
-elif [[ $MODE == "SCSS" ]]; then 
+elif [[ $FLAVOR == "SCSS" ]]; then 
 	# Svelte + SCSS 
 	svinstall 'yajatvishwak/SvelteSCSS' $PROJECT_NAME
-elif [[ $MODE == "Express" ]]; then 
+elif [[ $FLAVOR == "Express" ]]; then 
 	# Svelte + ExpressJS + SCSS
 	svinstall 'synapsecode/SvelteExpress' $PROJECT_NAME
+elif [[ $FLAVOR == "Tailwind" ]]; then 
+	# Svelte + TailwindCSS
+	svinstall 'synapsecode/Svelte-TailwindTemplate' $PROJECT_NAME
 else
-	echo "❌ Invalid Mode"
+	echo "❌ Invalid Svelte Flavor"
 fi
 
